1b1eb9452b057c40f703e67f565d2a900a292536
midhun9 <midhun.koneru111@gmail.com>
2021-08-30 13:19:54 -0600

    Initial Commit

---
+++ finder-app/finder.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# finder script to find number of files and number of matching lines with the search string passed
+# Paramaeters : first argument  - path to a directory on a file system
+#		second argument - text string which will be searched within the files in the above directory 
+# Author: Midhun Kumar Koneru
+
+filesdir="$1"
+searchstr="$2"
+
+if [ $# -eq 2 ]; then
+	if [ ! -d $1 ]; then
+		echo "passed directory does not exist"
+		exit 1
+	fi
+else
+	echo "Number of Arguments invalid: should be equal to 2"
+	exit 1
+fi
+
+function total_num_of_files() {
+	find $filesdir -type f | wc -l
+}
+
+function total_num_matching_lines() {
+	grep -R $searchstr $filesdir | wc -l
+}
+
+total_files=$(total_num_of_files)
+matching_lines=$(total_num_matching_lines)
+
+#echo "The number of files are $(find $1 -type f | wc -l) and the number of matching lines are $(grep -R $2 $1 | wc -l)" #instead of functions we can directly use this line to get the results
+echo "The number of files are $total_files and the number of matching lines are $matching_lines"
---
+++ finder-app/writer.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# writer script to write the text string passed to the file passed as first argument. If the file does not exist create a new file and write the string
+# Paramaeters : first argument  - full path to a file (including filename) on the filesystem
+#               second argument - text string which will be written in the file specified above 
+# Author: Midhun Kumar Koneru
+
+
+
+writefile="$1"
+DIR="`dirname "$1"`"
+writestr="$2"
+
+mkdir -p "$DIR"
+
+if [ $# -eq 2 ]; then
+	if [ ! -f "$writefile" ]; then
+		echo "passed file does not exist, creating new file"
+		touch "$writefile"
+	fi
+else
+        echo "Number of Arguments invalid: should be equal to 2"
+        exit 1
+fi
+
+if [ ! -f $writefile ]; then
+	echo "couldnot create requested file"
+	exit 1
+fi
+
+echo $writestr > $writefile
+
+echo "Text written successfully"
+
