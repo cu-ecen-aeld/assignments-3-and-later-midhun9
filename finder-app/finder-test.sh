f351217b861d839e20001bcc7b72cbbb3e8f8512
midhun9 <midhun.koneru111@gmail.com>
2021-09-18 21:26:49 -0600

    finder-test change path

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,7 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
 FINDEROUTPUTDIR=/tmp/assignment-4-result.txt
 
 if [ $# -lt 2 ]
